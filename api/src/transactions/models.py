c3478552b92304f0cfa9cdc939efe13fccaa73c4
cdbullones <cbullones@arawato.co>
2018-09-05 14:39:29 -0400

    Migracion campos transaction_detail

--- api/src/transactions/models.py
+++ api/src/transactions/models.py
@@ -5,10 +5,11 @@ class Transaction(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     investment = models.DecimalField(max_digits=19, decimal_places=10) 
     earnings   = models.DecimalField(max_digits=19, decimal_places=10)
-    
+
 class TransactionDetail(models.Model):
     transaction = models.ForeignKey(Transaction, null=True, blank=True, on_delete=models.CASCADE, related_name='details')
     parity = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=19, decimal_places=10)
     commission = models.DecimalField(max_digits=19, decimal_places=10)
     okex_order = models.IntegerField(default=0)
+    transaction_order = models.IntegerField(default=0)
