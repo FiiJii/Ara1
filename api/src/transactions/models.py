ed63ccc2f1f5d09c0e72b0ae29f703a8efa60ef8
cdbullones <cbullones@arawato.co>
2018-09-05 14:24:13 -0400

    Nueva migracion datos transaction

--- api/src/transactions/models.py
+++ api/src/transactions/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 class Transaction(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
-
-
+    investment = models.DecimalField(max_digits=19, decimal_places=10) 
+    earnings   = models.DecimalField(max_digits=19, decimal_places=10)
+    
 class TransactionDetail(models.Model):
     transaction = models.ForeignKey(Transaction, null=True, blank=True, on_delete=models.CASCADE, related_name='details')
     parity = models.CharField(max_length=100)
