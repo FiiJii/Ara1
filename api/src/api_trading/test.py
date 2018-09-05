ed8044fcab43f07ba8ae04c71ce49e4f47c3994f
cdbullones <cbullones@arawato.co>
2018-09-05 10:47:26 -0400

    refactoring tests

---
+++ api/src/api_trading/test.py
@@ -0,0 +1,23 @@
+from rest_framework.test import APILiveServerTestCase, APITestCase
+
+class TradingBaseTestCase(APILiveServerTestCase):
+
+    def get_url_server(self):
+        return self.live_server_url+"/"
+    
+    def setupUser(self):
+        self.username = "john"
+        self.email = "john@snow.com"
+        self.password = "you_know_nothing"
+        self.user = User.objects.create_user(self.username, self.email, self.password)
+
+    def setupToken(self):
+        url = self.get_url_server()+"api/auth/token/"
+        data = {"username":self.username,"password": self.password}
+        response = requests.post(url,json=data)
+        self.assertEqual(200, response.status_code)
+        self.assertTrue("access" in json.loads(response.content))
+        self.auth = response.json()
+        self.token = self.auth['access']
+ 
+    
--- api/src/transactions/tests.py
+++ api/src/transactions/tests.py
@@ -2,21 +2,16 @@ from django.test import TestCase
 from rest_framework.test import APILiveServerTestCase, APITestCase
 import requests
 import json
+from api_trading.test import TradingBaseTestCase
 from django.contrib.auth.models import User
 from transactions.models import Transaction
 
 
-class UserLoginTokenTestCase(APILiveServerTestCase):
+class UserLoginTokenTestCase(TradingBaseTestCase):
 
-    def get_url_server(self):
-        return self.live_server_url+"/"
     
     def setUp(self):
-        self.username = "john"
-        self.email = "john@snow.com"
-        self.password = "you_know_nothing"
-        self.user = User.objects.create_user(self.username, self.email, self.password)
-    
+       self.setupUser();   
     def test_authentication_without_password(self):
         url = self.get_url_server()+"api/auth/token/"
         response = requests.post(url, {"username":"snowman"})
@@ -34,44 +29,18 @@ class UserLoginTokenTestCase(APILiveServerTestCase):
         response = requests.post(url,json=data)
         self.assertEqual(200, response.status_code)
         self.assertTrue("access" in json.loads(response.content))
-        self.token = response.json()
-        #print(self.token['access'])  
     
-class TransactionsTestCase(UserLoginTokenTestCase):
+class TransactionsTestCase(TradingBaseTestCase):
 
-    def get_url_server(self):
-        return self.live_server_url+"/"
-    
-        """
-        So create user
-        """
     def setUp(self):
-        self.username = "john"
-        self.email = "john@snow.com"
-        self.password = "you_know_nothing"
-        self.user = User.objects.create_user(self.username, self.email, self.password)
-        self.test_authentication_with_valid_data()
-    
+        self.setupUser();
+        self.setupToken();
         """
         Test to verify the create a token
         """    
-    def get_token(self):
-        url = self.get_url_server()+"api/auth/token/"
-        data = {"username":self.username,"password": self.password}
-        response = requests.post(url,json=data)
-        self.assertEqual(200, response.status_code)
-        self.assertTrue("access" in json.loads(response.content))
-        self.auth = response.json()
-        self.token = self.auth['access']
         #print(self.token)
 
     def test_register_transaction(self):
-        """
-        Get 'access' token 
-        """
-        self.get_token()
-        self.token = self.auth['access']
-
         """
         Test to verify the create a transaction
         """
@@ -81,16 +50,17 @@ class TransactionsTestCase(UserLoginTokenTestCase):
         response = requests.post(url,headers=header)
         self.assertEqual(201, response.status_code)
 
-    def test_query_all_transaction(self):
-        """
-        Get 'access' token 
-        """
-        self.get_token()
-        self.token = self.auth['access']
+class TransactionQueryTestCase(TradingBaseTestCase):
 
-        self.test_register_transaction()
-        self.test_register_transaction()
+    def setUp(self):
+        self.setupUser();
+        self.setupToken();
+        for i in range(2):
+            url = self.get_url_server()+"api/trading/transactions/"
+            header = {'Authorization':'Bearer '+str(self.token)}
+            response = requests.post(url,headers=header)
         
+    def test_query_all_transaction(self):
         """
         Test to verify transactions queries already created
         """
@@ -103,14 +73,25 @@ class TransactionsTestCase(UserLoginTokenTestCase):
         #print(data)
         self.assertEqual(data["count"],2)
 
+
+
+class TransactionDetailsTestCase(TradingBaseTestCase):
+
+    def setUp(self):
+        self.setupUser();
+        self.setupToken();
+        url = self.get_url_server()+"api/trading/transactions/"
+        header = {'Authorization':'Bearer '+str(self.token)}
+        response = requests.post(url,headers=header)
+        self.transaction_url=response.json["url"]
+    
     def test_register_detail_transaction(self):
         """
         Get 'access' token 
         """
-        self.get_token()
+        self.setupToken()
         self.token = self.auth['access']
 
-        self.test_register_transaction()
 
         url = self.get_url_server()+"api/trading/transaction_details/"
         header = {'Authorization':'Bearer '+str(self.token)}
@@ -125,5 +106,4 @@ class TransactionsTestCase(UserLoginTokenTestCase):
         print(data)
         response = requests.post(url,headers=header,json=data)
         self.assertEqual(201, response.status_code)
-
 
